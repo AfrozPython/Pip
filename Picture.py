08072c25b2558e5f76dd55a818245823d5cd0b1a
Xyz <109797079+AfrozPython@users.noreply.github.com>
2023-01-05 18:52:30 +0530

    Add files via upload

---
+++ Picture.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  5 18:42:26 2023
+
+@author: Appu
+"""
+
+
+import streamlit as st
+# Video Upload
+video_file = open('C:/Users/Appu/Desktop/Streamlit Test/Karina3.png', 'rb')
+video_bytes = video_file.read()
+st.image(video_bytes)
+
+
+st.checkbox('yes')
+st.button('Click')
+st.radio('Pick your gender',['Male','Female'])
+st.selectbox('Pick your gender',['Male','Female'])
+st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+st.slider('Pick a number', 0,50)
